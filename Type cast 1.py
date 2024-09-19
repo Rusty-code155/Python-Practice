3799a689cebd1c53ec514311b5df56314d25b094
[Rusty-code155] <[PEEPLET@usca.edu]>
2024-09-19 00:12:14 -0400

    Continuation 9/18/2024

--- Type cast 1.py
+++ Type cast 1.py
@@ -1,1 +1,19 @@
 #Type Casting: Converts data types of a value to another data type
+x=1 #Type: int
+y=1.23 #Type: Float
+z=("Jeff") #Type: String
+response=True # Type Boolean
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(response))
+#Type cast
+print(type(str(x)))
+print(type(int(y)))
+print(type(str(response)))
+#Note: String only functions for the type cast if the variable value is equivalent to the type of the conversion
+#Perminate type cast
+x=float(x)
+y=int(y)
+print(type(x))
+print(type(y))
