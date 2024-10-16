87157d48b693ff2f15586dca21f38c5644da703b
[Rusty-code155] <[PEEPLET@usca.edu]>
2024-10-16 10:30:20 -0400

    String Slicing part 1

---
+++ String Slicing.py
@@ -0,0 +1,10 @@
+# slicing = creating a substring by extracting elements from another string
+#               indexing[] or slice()
+#                [start:stop:step]
+
+name = "Turner Miles"
+
+first_name= name[0:6]
+middle_name=name[7:13]
+print(first_name)
+print (middle_name)
