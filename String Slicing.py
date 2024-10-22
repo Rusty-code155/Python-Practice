a6e47c398ee19e2574d292dd68b1ae80897d612d
Rusty-code155 <PEEPLET@usca.edu>
2024-10-22 13:17:08 -0400

    Check

--- String Slicing.py
+++ String Slicing.py
@@ -3,8 +3,13 @@
 #                [start:stop:step]
 
 name = "Turner Miles"
-
+#Indexing
 first_name= name[0:6]
 middle_name=name[7:13]
 print(first_name)
 print (middle_name)
+funky_name=name[0:13:2]
+print(funky_name)
+reversed_name=name[::-1]
+print(reversed_name)
+#Slicing
