3dc36c2eae72589f7c836703b5b661edfc34541d
[Rusty-code155] <[PEEPLET@usca.edu]>
2024-10-28 11:39:34 -0400

    Check

--- Graph.py
+++ Graph.py
@@ -1,3 +1,13 @@
 import pandas as pd
+#Established to variable term used for pandas value and pulls the file into the sequence
 df=pd.read_excel("Al2O3 _W Viscosities.xlsx")
-print(df)
+
+df.columns
+#Prints the table values into the terminal. .head function specifies the reference location and the (5) tells the system how many rows you wish to pull from the excel sheet
+    #print(df.head(5))
+#Read Headers
+    #print(df.columns)
+#Read each Column
+    #print(df.Name)
+#Read each Row
+print(df.iloc[2])
